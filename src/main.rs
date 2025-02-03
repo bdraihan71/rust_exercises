13a1b301eba2023594ccf4c5931c3541a5e807e4
Raihan Farhad <bdraihan71@gmail.com>
2025-02-03 22:48:08 +0600

    add Uninitialized variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    //    let x;
+    //    foobar(x);
+    //    x = 42;
+
+    let x;
+    x = 42;
+    foobar(x);
+}
+
+fn foobar(x: i32) {
+    println!("x = {}", x);
 }
