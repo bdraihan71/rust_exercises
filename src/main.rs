3fc8ec5ea4d4dd811191ec6182c56f75e175c585
Raihan Farhad <bdraihan71@gmail.com>
2025-02-03 22:54:59 +0600

    add Throwing values away

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let _ = 42;
+    let _ = get_thing();
 }
+
+fn get_thing(x: i32) {
+   println!("x: {}", x);
+}
+// have some issues with the code above
